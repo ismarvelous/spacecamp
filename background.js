57407c866ce8dca2e866a51783a7ae55ea16c886
Jeroen Wijdeven <jeroen@marvelous.nl>
2020-08-26 13:05:23 +0200

    styling and label update

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
-
-(function() {
+(function($, global, document) {
 
 	var calculate = function () { // progress calculation..
 
@@ -22,7 +21,6 @@
 				}
 			});
 
-
 			$(this).find("ul.completed a").each(function (index) {
 
 				var matches = regExp.exec($(this).text());
@@ -40,33 +38,22 @@
 		});
 	};
 
-	var labelit = function() { // label it
+	var labelit = function() { // label its
 
-		const timeChimpUrl = "https://app.timechimp.com/?text={text}#/registration/time/day";
-
-		var labels = "";
+		const timeUrlPattern = "https://app.timechimp.com/?text={labels}%20|%20{title}%20|%20Opmerkingen%3A#/registration/time/day";
+		
+		var labels = [];
 
 		var appendLabel = function(html)
 		{
-			var re = /(#(\w+)([-](\w+))*)/g, match, matches = [];
-			
-			while (match = re.exec(html)) {
-  				matches.push(match[1]);
-			}
+			var re = /(#(\w+)([-](\w+))*)/g;
 
-			for(i=0; i<matches.length; i++)
-			{
-				if(timeChimpUrl.length > 0)
-				{
-					var url = timeChimpUrl.replace("{text}", matches[i].replace("#", "%23"));
-					labels = labels +  "<a href='"+ url +"'>" + matches[i] + "</a>";
-				}
-				else
-				{
-					labels = labels + matches[i];
-				}
+			var match = null;
+			while (match = re.exec(html)) {
+  				labels.push(match[1]);
 			}
-			console.log(labels);
+			
+			//console.log(labels);
 		}
 
 		$("div.thread-entry__content").each(function (index){
@@ -81,12 +68,17 @@
 			dom.find("div.thread-entry__content").each(function (index){
 				//console.log($(this).html());
 				appendLabel($(this).html());
-				console.log(labels);
+				//console.log(labels);
 			});
 		})
 		.always(function(){
 			
-			console.log("Finished!");
+			//console.log("Finished!");
+
+			var jLabels = labels.join(", ");
+			var url = timeUrlPattern;
+			url = url.replace("{labels}", jLabels.replaceAll("#", "%23").replaceAll(",", "%2C").replaceAll(" ", "%20"));
+			url = url.replace("{title}", $(document).find("title").text());
 
 			var html = `
 				<div class="todos-form__field">
@@ -94,7 +86,19 @@
 						<strong>Labels</strong>
 					</div>
 					<div class="todos-form__field-content">
-							<div>`+ labels +`</div>
+							<div>`+ jLabels + `</div>
+						</div>
+					</div>
+				</div>
+			`;
+
+			var html = html + `
+				<div class="todos-form__field">
+					<div class="todos-form__field-label todos-form__field-label--notes">
+						<strong>Track time</strong>
+					</div>
+					<div class="todos-form__field-content">
+							<div><a href="` + url + `" target="time"><img src="`+ chrome.extension.getURL("icon.png") + `" style="width: 25px; margin-top:10px"/></a></div>
 						</div>
 					</div>
 				</div>
@@ -114,27 +118,27 @@
 		$("textarea[ng-model='vm.time.notes']").val(urlParams.get("text"));
 	}
 	
-	var init = function()
+	global.initializeSpaceCamp = function()
 	{
-		var isBasecamp = window.location.hostname.toLowerCase() !== "app.timechimp.com";
+		var isTimechimp = window.location.hostname.toLowerCase() === "app.timechimp.com";
 
-		if(isBasecamp) //basecamp
+		if(isTimechimp)
+		{
+			addDescription();
+		}
+		else //basecamp
 		{
 			calculate(); //run calculate onload..
 			labelit();
 		}
-		else //timechimp
-		{
-			addDescription();
-		}
 	};
 
+	
 
+})(jQuery, window, document);
 
-	$(document).ready(function() {
-		init();
-	});
-
-})();
+$(document).ready(function() {
+	window.initializeSpaceCamp();
+});
 
 
